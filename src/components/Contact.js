b4553c4e283ca0319300a1bedc13adc567a98037
ghendersonn <hgavin32@gmail.com>
2021-06-25 19:04:30 -0400

    secondary color

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,93 +19,88 @@ export default class Contact extends Component {
             </Helmet>
 
             <main>
-            <div class="color">
-            <div class="mainwrapper2"
-                >
+            
+            <div class="mainwrapper2">
             
                 <div class="col1">
+                    <ul className="topleft">
+                        <li><Link to='./home' style={{ textDecoration: 'none' }} ><span className="active"  data-text="intro">intro</span></Link></li> 
+                        <li> <Link to='./users' style={{ textDecoration: 'none' }} ><span  data-text="work">work</span></Link></li> 
+                        <li><Link to='./team' style={{ textDecoration: 'none' }} ><span  data-text="team">team</span></Link></li> 
+                        <li><Link to='./contact' style={{ textDecoration: 'none' }} ><span  data-text="contact">contact</span></Link></li> 
+                    </ul>
+                </div>
                 
-                <ul className="topleft">
-         <li><Link to='./home' style={{ textDecoration: 'none' }} ><span className="active"  data-text="intro">intro</span></Link></li> 
-         <li> <Link to='./users' style={{ textDecoration: 'none' }} ><span  data-text="work">work</span></Link></li> 
-         <li><Link to='./team' style={{ textDecoration: 'none' }} ><span  data-text="team">team</span></Link></li> 
-         <li><Link to='./contact' style={{ textDecoration: 'none' }} ><span  data-text="contact">contact</span></Link></li> 
-        </ul>
-                
-               </div>
-                
-
-            
                 <div class="col2con"> 
-                <h1  class="mainbanner">contact</h1>
+                    <h1  class="mainbanner">contact</h1>
                 
-                <form name="contact" method="post">
-                <input type="hidden" name="form-name" value="contact" />
-                    <div class="containerform">
-                    <div class="row100">
-                        <div class="col">
-                        <div class="inputBox">
-                            <input type="text" name="firstname" required="required"/>
-                            <span class="text">First Name</span>
-                            <span class="line"></span>
-                        </div>
+                    <form name="contact" method="post">
+                        <input type="hidden" name="form-name" value="contact" />
+                        <div class="containerform">
+                            <div class="row100">
+                                <div class="col">
+                                    <div class="inputBox">
+                                        <input type="text" name="firstname" required="required"/>
+                                        <span class="text">First Name</span>
+                                        <span class="line"></span>
+                                    </div>
+                            
+                                </div>
+                            
+                                <div class="col">
+                                    <div class="inputBox">
+                                        <input type="text" name="lastname" required="required"/>
+                                        <span class="text">Last Name</span>
+                                        <span class="line"></span>
+                                    </div>
+                            
+                                </div>
                         
+                            </div>
+                
+                            <div class="row100">
+                                <div class="col">
+                                    <div class="inputBox">
+                                        <input type="text" name="email" required="required"/>
+                                        <span class="text">Email</span>
+                                        <span class="line"></span>
+                                    </div>
+                                
+                                </div>
+                                
+                                <div class="col">
+                                    <div class="inputBox">
+                                        <input type="text" name="mobile" required="required"/>
+                                        <span class="text">Mobile</span>
+                                        <span class="line"></span>
+                                    </div>
+                                
+                                </div>
+                            
+                            </div>
+                
+                            <div class="row100">
+                                <div class="col">
+                                    <div class="inputBox textarea">
+                                        <textarea name="message" required="required"></textarea>
+                                        <span class="text">Type Your Message Here...</span>
+                                        <span class="line"></span>
+                                    </div>
+                                
+                                </div>
+                            
+                            </div>
+                            
+                            <div class="row100">
+                                <div class="col">
+                                    <button type="submit" value="Send">Submit</button>
+                                </div>
+                            
+                            </div>
+                
                         </div>
-                        
-                        <div class="col">
-                        <div class="inputBox">
-                            <input type="text" name="lastname" required="required"/>
-                            <span class="text">Last Name</span>
-                            <span class="line"></span>
-                        </div>
-                        
-                        </div>
-                        
-                    </div>
-                    
-                    <div class="row100">
-                        <div class="col">
-                        <div class="inputBox">
-                            <input type="text" name="email" required="required"/>
-                            <span class="text">Email</span>
-                            <span class="line"></span>
-                        </div>
-                        
-                        </div>
-                        
-                        <div class="col">
-                        <div class="inputBox">
-                            <input type="text" name="mobile" required="required"/>
-                            <span class="text">Mobile</span>
-                            <span class="line"></span>
-                        </div>
-                        
-                        </div>
-                    
-                    </div>
-                    
-                    <div class="row100">
-                        <div class="col">
-                        <div class="inputBox textarea">
-                            <textarea name="message" required="required"></textarea>
-                            <span class="text">Type Your Message Here...</span>
-                            <span class="line"></span>
-                        </div>
-                        
-                        </div>
-                    
-                    </div>
-                    
-                    <div class="row100">
-                        <div class="col">
-                            <button type="submit" value="Send">Submit</button>
-                        </div>
-                    
-                    </div>
-                    
-                    </div>
-                    
-                </form>
+                
+                    </form>
                 
                 </div>
             
@@ -158,7 +153,7 @@ export default class Contact extends Component {
       </div>
       </div>
     </div>
-            </div>
+           
             </main>  
             </div>
             )
